2c0fa1d7279ae25490ff2914194e8acd480b0914
asem-alahmar <1abomazen1@gmail.com>
2021-01-02 14:12:18 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
-import axios from 'axios'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route, Link } from "react-router-dom";
 
 import Card from './components/Card'
-import {lightTheme, darkTheme} from './Dark theme/cardTheme'
-import { lightTheme, darkTheme } from "./Dark theme/Theme";
+import {lightTheme, darkTheme} from './Dark theme/cardTheme';
 
 import Asem from './components/Asem';
 import Main from './components/Main';
@@ -20,11 +12,6 @@ import Achievements from './components/Achievements';
 import Footer from './components/Footer';
 
 
-
-const BEARER_TOKEN= 'Xn6NPFz3k-y7dtRic1KM';
-axios.defaults.baseURL = 'https://the-one-api.dev/v2/';
-axios.defaults.headers.common['Authorization'] = "Bearer "+BEARER_TOKEN;
-axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
 
@@ -36,12 +23,9 @@ function App(){
   }
   const colSize = 6
   return (
-    
-    
     <Router>
       <React.Fragment>
-      <Header />
-        
+      <Header /> 
         <Switch>
 
         <Route exact path="/">
